41a2ad817f17b1df71d5e1f54ef96cb026976224
kiarn <arnaud@linuxmuster.net>
2025-12-11 14:20:19 +0100

    Better quotas display.

--- usr/lib/python3/dist-packages/linuxmusterCli/typers/quotas.py
+++ usr/lib/python3/dist-packages/linuxmusterCli/typers/quotas.py
@@ -38,21 +38,32 @@ def ls(
         title_suffix = f"of schoolclass {schoolclass}"
         users = lr.getval(f'/schoolclasses/{schoolclass}', 'sophomorixMembers')
 
-    quotas = Table(title=f"Quotas {title_suffix}")
-    quotas.add_column("User", style="cyan")
-    quotas.add_column("Used global", style="yellow")
-    quotas.add_column(f"Used {school}", style="green")
-    quotas.add_column("Cloud", style="green")
-    quotas.add_column("Mail", style="green")
+    table_quotas = Table(title=f"Quotas {title_suffix}")
+    table_quotas.add_column("User", style="cyan")
+    table_quotas.add_column("Used global", style="yellow")
+    table_quotas.add_column(f"Used {school}", style="green")
+    table_quotas.add_column("Cloud", style="green")
+    table_quotas.add_column("Mail", style="green")
 
-    data = [[c.header for c in quotas.columns]]
+    data = [[c.header for c in table_quotas.columns]]
 
     def add_quotas(user):
-        user_quotas = get_user_quotas(user)
-        print(user, user_quotas)
+        quotas = get_user_quotas(user)
 
-        data.append([user, user_quotas['linuxmuster-global']['used'], user_quotas[school]['used'], user_quotas['cloud'], user_quotas['mail']])
-        quotas.add_row(user, str(user_quotas['linuxmuster-global']['used']), str(user_quotas[school]['used']), user_quotas['cloud'], user_quotas['mail'])
+        data.append([
+            user,
+            f"{quotas['linuxmuster-global']['used']}/{quotas['linuxmuster-global']['hard_limit']}",
+            f"{quotas[school]['used']}/{quotas[school]['hard_limit']}",
+            quotas['cloud'],
+            quotas['mail']
+        ])
+        table_quotas.add_row(
+            user,
+            f"{quotas['linuxmuster-global']['used']:<9} / {quotas['linuxmuster-global']['hard_limit']}",
+            f"{quotas[school]['used']:<9} / {quotas[school]['hard_limit']}",
+            quotas['cloud'],
+            quotas['mail']
+        )
 
     with futures.ThreadPoolExecutor() as executor:
         infos = executor.map(add_quotas, users)
@@ -60,4 +71,4 @@ def ls(
     if state.format:
         printf.format(data)
     else:
-        console.print(quotas)
+        console.print(table_quotas)
